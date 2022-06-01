119598a85c742f2b35475f469eceff3b93c5b3c4
alipala.ist@gmail.com <alipala.ist@gmail.com>
2022-06-01 22:30:57 +0200

    Updated TestSuite

--- src/test/java/gitlab/TestSuite.java
+++ src/test/java/gitlab/TestSuite.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         plugin = {"pretty"},
-        features = {"src/test/resources/features/edge/TC007_Destructive_Tests.feature"}
+        features = {"src/test/resources/features"}
 )
 
 public class TestSuite {
